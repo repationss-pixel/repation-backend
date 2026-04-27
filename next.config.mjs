44c18a14cc8886d65dfb25fe5c260413ada1f55c
MacBook <macbook@MacBookPro.lan>
2026-04-27 17:56:57 +0200

    Fix Prisma Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+  },
 };
 
 export default nextConfig;
