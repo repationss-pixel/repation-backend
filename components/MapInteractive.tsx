fc9686f381d09c0fb7c13376047b3bc32784e2d6
MacBook <macbook@MacBookPro.lan>
2026-05-04 11:58:36 +0200

    Carte iframe simple OpenStreetMap

--- components/MapInteractive.tsx
+++ components/MapInteractive.tsx
@@ -2,10 +2,18 @@
 
 export default function MapInteractive() {
   return (
-    <iframe
-      src="https://www.openstreetmap.org/export/embed.html?bbox=-5.0,41.0,10.0,51.5&layer=mapnik"
-      style={{ width: '100%', height: '500px', border: 'none', borderRadius: '16px' }}
-      title="Carte Repation"
-    />
+    <div style={{ width: '100%', height: '500px', borderRadius: '16px', overflow: 'hidden' }}>
+      <iframe
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        scrolling="no"
+        marginHeight={0}
+        marginWidth={0}
+        src="https://www.openstreetmap.org/export/embed.html?bbox=-5.142,41.333,9.560,51.124&layer=mapnik"
+        style={{ border: 'none' }}
+        title="Carte Repation"
+      />
+    </div>
   )
 }
