ec9134a33cf6902ec863bd900a210161d66925fc
MacBook <macbook@MacBookPro.lan>
2026-05-14 11:56:27 +0200

    Fix CSP Nominatim autocomplétion

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,7 @@ const nextConfig = {
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: blob: https://*.tile.openstreetmap.org https://*.supabase.co",
               "frame-src https://www.openstreetmap.org",
-              "connect-src 'self' https://*.supabase.co https://api.resend.com",
+              "connect-src 'self' https://*.supabase.co https://api.resend.com https://nominatim.openstreetmap.org",
             ].join('; '),
           },
         ],
